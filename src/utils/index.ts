2631b5eae9f28904ebdd14b082b774a1172ddcdd
raihan-778 <raihanarif778@gmail.com>
2023-05-11 00:19:16 +0600

    new topic added

---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+//re-export methodology.
+
+import add2 from "./add";
+import subtract from "./subtract";
+import multiply from "./multiply";
+import divided from "./divided";
+import average from "./average";
+
+export default {
+  add2,
+  subtract,
+  multiply,
+  divided,
+  average,
+};
